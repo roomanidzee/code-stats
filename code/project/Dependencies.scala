937c1755b0ff503b6b8a2b813ca501c9096a9926
roomanidzee <steampart@gmail.com>
2020-09-13 12:55:41 +0300

    fixed deps

--- code/project/Dependencies.scala
+++ code/project/Dependencies.scala
@@ -21,14 +21,8 @@ object Dependencies {
   private val logging: Seq[ModuleID] = Seq(
     "org.apache.logging.log4j" % "log4j-api" % Versions.log4j2,
     "org.apache.logging.log4j" % "log4j-core" % Versions.log4j2,
+    "org.apache.logging.log4j" % "log4j-1.2-api" % Versions.log4j2,
     "com.typesafe.scala-logging" %% "scala-logging" % Versions.scalaLogging
-  )
-
-  private val relationalDB: Seq[ModuleID] = Seq(
-    "org.tpolecat" %% "doobie-core"   % Versions.doobie,
-    "org.tpolecat" %% "doobie-h2"     % Versions.doobie,
-    "org.tpolecat" %% "doobie-hikari" % Versions.doobie,
-    "com.h2database" % "h2" % Versions.h2
   )
 
   private val fs2: Seq[ModuleID] = Seq(
@@ -61,9 +55,6 @@ object Dependencies {
 
   val parquetModuleDeps: Seq[ModuleID] = monix.union(monixParquet).union(logging)
   val parquetModuleTestDeps: Seq[ModuleID] = munit.union(logging)
-
-  val dbModuleDeps: Seq[ModuleID] = monix.union(logging).union(relationalDB).union(fs2)
-  val dbModuleTestDeps: Seq[ModuleID] = munit.union(logging)
 
   val webModuleDeps: Seq[ModuleID] =
     monix.union(logging)
